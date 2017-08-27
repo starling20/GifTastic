48ffb71241aee3d13f4eb294a4039f4a2edd3466
Lauren Starling <laurenstarling20@icloud.com>
2017-08-26 22:45:42 -0600

    adding files

---
+++ gf.js
@@ -0,0 +1,66 @@
+body {
+margin-top: 75px;
+ margin-bottom: 75px;
+ background-image: url("gradient.png");
+ background-size: cover;
+ text-align: center;
+font-family: 'Open Sans', sans-serif;
+ text-transform: uppercase;
+ }
+ 
+ .container {
+ 	
+ 	width: ;
+ }
+ 
+ h1 {
+	font-family: 'Fredoka One', cursive;
+	font-size: 55px;
+	color: #fff;
+}
+ 
+
+h3 {
+	font-family: 'Open Sans', sans-serif;
+	font-size: 15px;
+	color: #fff;
+}
+
+form {
+	color: black;
+}
+
+#buttonsView {
+	display: inline;
+}
+
+.gifs {
+	display: inline;
+}
+
+#gifsView {
+ 	display: inline;
+	margin-bottom: 40px;
+}
+ 
+.gif {
+ 	cursor: crosshair;
+ }
+ 
+img.squiggle {
+ 	top:0;
+ 	left:0;
+ }
+ 
+#show-form {
+ 	display: inline;
+ }
+ 
+ button {
+ 	background: #fff;
+ 	margin: 5px;
+ 	font-family: 'Open Sans', sans-serif;
+ 	color: black;
+ }
+ 
+ 
